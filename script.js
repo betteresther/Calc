a5dee65fc233efc695e0d58cef6ac6c81dc00cb6
betteresther <orijaesther10@gmail.com>
2023-07-25 16:01:10 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// display() 
+function display(val){
+
+    document.getElementById('result').value += val
+
+    return val
+
+}
+// solve()
+function solve(){
+
+    let x = document.getElementById('result').value
+
+    let y = eval(x);
+
+    document.getElementById('result').value = y
+
+    return y
+
+}
+
+function clearScreen(){
+
+    document.getElementById('result').value = ''
+
+}
